48c4fd03c30b79dda0d69eb8c074608db2411a08
Sohang Chopra <sohangchopra@gmail.com>
2023-02-21 18:55:13 +0530

    Uploaded to Test Pypi

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
-from distutils.core import setup
-from distutils.command.install import install
+from setuptools import setup
+from setuptools.command.install import install
 import sys
+import os
 import shutil
 
 class install_with_kernelspec(install):
     def run(self):
         install.run(self)
         from jupyter_client.kernelspec import install_kernel_spec
-        install_kernel_spec('kernelspec', 'forth', replace=True)
+        project_dir = os.path.dirname(os.path.realpath(__file__))
+        install_kernel_spec(os.path.join(project_dir, 'kernelspec'), 'forth', replace=True)
 
 with open('README.md') as f:
     readme = f.read()
@@ -17,16 +19,21 @@ if svem_flag in sys.argv:
     # Die, setuptools, die.
     sys.argv.remove(svem_flag)
 
+packages_required = ['pexpect>=3.3', 'jupyter_client', 'ipykernel']
+
 setup(name='forth_kernel',
-      version='0.2',
-      description='A Forth kernel for IPython',
+      version='0.3',
+      description='A Forth kernel for IPython (Fork of https://github.com/jdfreder/iforth)',
       long_description=readme,
-      author='Jonathan Frederic',
-      author_email='jon.freder@gmail.com',
-      url='https://github.com/jdfreder/iforth',
+      long_description_content_type='text/markdown',
+      include_package_data=True,
+      author='Sohang Chopra',
+      author_email='sohangchopra@gmail.com',
+      url='https://github.com/sohang3112/iforth',
       py_modules=['forth_kernel'],
       cmdclass={'install': install_with_kernelspec},
-      install_requires=['pexpect>=3.3'],
+      setup_requires=packages_required,
+      install_requires=packages_required,
       classifiers = [
           'Framework :: IPython',
           'License :: OSI Approved :: BSD License',
